00cd693890e2d7ccda0344eb0e30e4de452d59c6
Lars Veelaert <lveelaert@gmail.com>
2017-11-07 17:49:41 +0000

    logs

--- download_captive_portal.py
+++ download_captive_portal.py
@@ -1,6 +1,10 @@
+#TODO bash notifier
+#TODO notice to run as python3
+
 from subprocess import Popen, PIPE, STDOUT
 
-cmd = 'ls /etc/fstab /etc/non-existent-file'
+cmd = 'curl -s -L www.google.com/index.html'
+#TODO detect fail
 p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
 output = p.stdout.read()
 print(output)
